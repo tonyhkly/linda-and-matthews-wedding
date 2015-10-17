39941321391d33372e8aec127c159b4cc6083d28
Tony Ly <tonyhkly@gmail.com>
2015-10-17 18:46:48 +0100

    Add parallax

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,12 @@ router.get('/page2', function(req, res, next) {
   res.render('page2', { title: 'Tony Hao Kin Ly'}); //Add layout: false to not use template
 });
 
+router.get('/page2', function(req, res, next) {
+  res.render('page2', { title: 'Tony Hao Kin Ly'}); //Add layout: false to not use template
+});
 
-router.get('/skroll', function(req, res, next) {
-  res.render('skrollr', { title: 'Tony Hao Kin Ly', layout: false}); //Add layout: false to not use template
+router.get('/prac', function(req, res, next) {
+  res.render('prac', { title: 'Tony Hao Kin Ly'}); //Add layout: false to not use template
 });
 
 module.exports = router;

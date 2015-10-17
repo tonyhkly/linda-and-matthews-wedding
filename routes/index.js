ea5376ba12489db92a5462c8746218b8b28c18b4
Tony Ly <tonyhkly@gmail.com>
2015-10-17 13:24:24 +0100

    Add Skrollr Support

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,9 @@ router.get('/page2', function(req, res, next) {
   res.render('page2', { title: 'Tony Hao Kin Ly'}); //Add layout: false to not use template
 });
 
+
+router.get('/skroll', function(req, res, next) {
+  res.render('skrollr', { title: 'Tony Hao Kin Ly', layout: false}); //Add layout: false to not use template
+});
+
 module.exports = router;

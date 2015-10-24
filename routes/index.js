2404a4d91fbfc9c52921c05d0b6e996ca59802d4
Tony Ly <tonyhkly@gmail.com>
2015-10-24 17:36:14 +0100

    Add Wedding poc

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,20 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/page2', function(req, res, next) {
-  res.render('page2', { title: 'Tony Hao Kin Ly'}); //Add layout: false to not use template
+  res.render('page2', { title: 'Tony Hao Kin Ly'});
 });
 
 router.get('/page2', function(req, res, next) {
-  res.render('page2', { title: 'Tony Hao Kin Ly'}); //Add layout: false to not use template
+  res.render('page2', { title: 'Tony Hao Kin Ly'});
 });
 
 router.get('/prac', function(req, res, next) {
-  res.render('prac', { title: 'Tony Hao Kin Ly'}); //Add layout: false to not use template
+  res.render('prac', { title: 'Tony Hao Kin Ly'});
 });
+
+router.get('/wedding', function(req, res, next) {
+  res.render('wedding', { title: 'Wedding', layout: false});
+});
+
 
 module.exports = router;

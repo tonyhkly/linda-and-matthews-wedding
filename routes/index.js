3a3639f8ad23ba244cfdf6cd49021f3e85b0ea8d
tonyhkly <tonyhkly@gmail.com>
2015-10-31 17:33:13 +0000

    Refactor and clean up

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Tony Hao Kin Ly'}); //Add layout: false to not use template
 });
 
-router.get('/page2', function(req, res, next) {
-  res.render('page2', { title: 'Tony Hao Kin Ly'});
-});
-
-router.get('/page2', function(req, res, next) {
-  res.render('page2', { title: 'Tony Hao Kin Ly'});
-});
-
 router.get('/prac', function(req, res, next) {
-  res.render('prac', { title: 'Tony Hao Kin Ly'});
+  res.render('prac', { title: 'Tony Hao Kin Ly', layout: false});
 });
 
 router.get('/wedding', function(req, res, next) {
-  res.render('wedding', { title: 'BBC - Home', layout: false});
+  res.render('wedding', { title: 'Home', layout: false});
 });
 
 module.exports = router;

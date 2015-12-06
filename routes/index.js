1fc791971cdf1756a073fbcaa31f07c0d75222cb
Tony Ly <Tony.Ly@Rightmove.co.uk>
2015-12-06 12:00:27 +0000

    Rename to Michael and Anna Web

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/prac', function(req, res, next) {
-  res.render('prac', { title: 'Tony Hao Kin Ly', layout: false});
+  res.render('prac', {
+      title: 'Tony Hao Kin Ly',
+      layout: false
+  });
 });
 
 router.get('/', function(req, res, next) {

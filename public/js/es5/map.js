10ce244a478347e2cf1daec11224e4ee7601663b
Tony Ly <Tony.Ly@Rightmove.co.uk>
2015-12-24 18:34:28 +0000

    Map info window

--- public/js/es5/map.js
+++ public/js/es5/map.js
@@ -9,11 +9,22 @@ function initMap() {
         draggable: !isMobile()
     });
 
+    var contentString = '<div id="info-window">' + '<h4>Pembroke Lodge</h4>' + '<a href="https://goo.gl/maps/pQdAzbBEjL62" target="_blank"><img class="img-responsive" src="/images/pembroke.jpg"></a>' + '<ul class="action-list">' + '<li><a href="https://goo.gl/maps/pQdAzbBEjL62" target="_blank">See on Google maps</a></li>' + '<li><a href="https://goo.gl/maps/aUs2Cu8RqLm" target="_blank">Get directions</a></li>' + '</ul>' + '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString,
+        maxWidth: 180
+    });
+
     var marker = new google.maps.Marker({
         position: latLong,
         map: map,
         animation: google.maps.Animation.DROP,
         title: 'Michael & Anna\'s Wedding venue!'
+    });
+
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
     });
 }
 

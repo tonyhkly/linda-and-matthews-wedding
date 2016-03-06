6b2fbd34df1f2a157a24101b9537af90f8366332
Tony Ly <Tony.Ly@Rightmove.co.uk>
2016-03-06 23:13:12 +0000

    Css fixes

--- public/js/es5/map.js
+++ public/js/es5/map.js
@@ -8,7 +8,7 @@ function initMap() {
 
     var map = new google.maps.Map(document.getElementById('map'), {
         center: latLong,
-        zoom: 14,
+        zoom: 15,
         draggable: !isMobile()
     });
 

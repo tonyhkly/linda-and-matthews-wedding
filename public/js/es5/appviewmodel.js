c8c7ecc93c7343295e73943e3002eeb37503d29b
Tony Ly <tony.ly@rightmove.co.uk>
2018-02-05 21:20:36 +0000

    Final thumbnail images and real address

--- public/js/es5/appviewmodel.js
+++ public/js/es5/appviewmodel.js
@@ -14,21 +14,11 @@ function photoDirectory() {
 }
 
 function AppViewModel() {
-    this.photos = [new Photo("manna-1.jpeg"), new Photo("manna-2.jpeg"), new Photo("manna-3.jpeg"), new Photo("manna-4.jpeg")];
+    this.photos = [new Photo("manna-1.jpeg"), new Photo("manna-3.jpeg"), new Photo("manna-2.jpeg"), new Photo("manna-4.jpeg")];
 
-    shuffle(this.photos);
+    // shuffle(this.photos);
 
     this.deviceClasses = addDeviceClasses();
-}
-
-function shuffle(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-    return array;
 }
 
 ko.applyBindings(new AppViewModel());

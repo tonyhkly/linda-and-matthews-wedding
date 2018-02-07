1590554e523ceb0b1e904a97d812996e78063af5
Tony Ly <Tony.Ly@Rightmove.co.uk>
2018-02-07 21:35:24 +0000

    Add map

--- public/js/es5/map.js
+++ public/js/es5/map.js
@@ -11,7 +11,7 @@ function initMap() {
         zoom: 15
     });
 
-    var contentString = '<div id="info-window">' + '<h4>De Vere Horsley Estate</h4>' + '<a href="https://goo.gl/maps/YHvbFPW7f9Q2" target="_blank">' + '<img class="img-responsive" src="/images/map-info-box.jpeg">' + '</a>' + '<ul class="action-list">' + '<li><a href="https://www.google.co.uk/maps/dir//De+Vere+Horsley+Estate,+Leatherhead/@51.2649807,-0.4994336,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x4875de83cebfa7d7:0x5c67ac743333475b!2m2!1d-0.429394!2d51.2650016" target="_blank">Get directions</a></li>' + '<li><a href="https://www.google.co.uk/maps/place/De+Vere+Horsley+Estate/@51.2650016,-0.429394,15z/data=!4m5!3m4!1s0x0:0x5c67ac743333475b!8m2!3d51.2650016!4d-0.429394" target="_blank">See on Google maps</a></li>' + '</ul>' + '</div>';
+    var contentString = '<div id="info-window">' + '<h4>De Vere Horsley Estate</h4>' + '<a href="https://www.google.co.uk/maps/place/De+Vere+Horsley+Estate/@51.2650016,-0.429394,15z/data=!4m5!3m4!1s0x0:0x5c67ac743333475b!8m2!3d51.2650016!4d-0.429394" target="_blank">' + '<img class="img-responsive" src="/images/map-info-box.jpeg">' + '</a>' + '<ul class="action-list">' + '<li><a href="https://www.google.co.uk/maps/dir//De+Vere+Horsley+Estate,+Leatherhead/@51.2649807,-0.4994336,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x4875de83cebfa7d7:0x5c67ac743333475b!2m2!1d-0.429394!2d51.2650016" target="_blank">Get directions</a></li>' + '<li><a href="https://www.google.co.uk/maps/place/De+Vere+Horsley+Estate/@51.2650016,-0.429394,15z/data=!4m5!3m4!1s0x0:0x5c67ac743333475b!8m2!3d51.2650016!4d-0.429394" target="_blank">See on Google maps</a></li>' + '</ul>' + '</div>';
 
     var infowindow = new google.maps.InfoWindow({
         content: contentString,
